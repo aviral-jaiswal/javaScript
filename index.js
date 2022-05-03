8df840684b8df301bb0f0d0b53d049323b0fe438
aviral-jaiswal <63544793+aviral-jaiswal@users.noreply.github.com>
2022-05-03 23:08:57 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -43,15 +43,88 @@
 // console.log(avi);
 
 //======================day 19==============================
-const obj ={
-    name: "avi",
-    email: "acs",
-    age : 20
+// const obj ={
+//     name: "avi",
+//     email: "acs",
+//     age : 20
+// }
+// console.log(obj.age);
+// let{name,email,age,phoneno="1234"}=obj;
+// console.log(`${name} and ${phoneno}`);
+
+// let add = function(a,b) // function as a variable
+// {
+//     return a+b;
+// }
+// console.log(add(20,30));
+
+// let Add= (x,y)=> x+y; // arrow function
+// console.log(Add(10,20)); 
+
+
+// //this keyword
+// const counter={
+//     count: 2,
+//     next : function()
+//     {
+//         return((this.count)*100);
+//     }
+// }
+// console.log(counter.next());
+//==========================day 20=======================
+// MAP 
+let arr=[1,2,3,4,5,6];
+// console.log(arr.map((e,index)=>e*100));
+
+/*difference btw map and forEach that map will create a new array for result whereas
+for each don't it will never return anything it will only print*/
+  
+
+// const newArr = arr.map( (value) => {
+//     if(value % 2 == 0){
+//         return value;
+//     }
+// })
+// console.log(newArr);
+
+// const newFilterArr = arr.filter( (value) => {
+//     if(value % 2 == 0){
+//         return value;
+//     }
+// })
+// console.log(newFilterArr);
+
+
+let concert = false;
+
+const attendConcert = new Promise( function (fulfill, reject){
+    setTimeout( () => {
+        if(concert){
+            fulfill("Atteded the Concert");
+        }else{
+            reject("Missed concert");
+        }
+    },1000);
+});
+
+// console.log(attendConcert);
+
+// console.log(attendConcert.then( (data) => console.log(data)));
+// console.log(attendConcert.catch( (error) => console.log(error)));
+// console.log(attendConcert.then( (data) => console.log(data)).catch( (error) => console.log(error)));
+
+// async-await
+const fun = async() =>{
+    try{
+        let  result = await attendConcert;
+        console.log(result);
+    }catch(e){
+        console.log(e);
+    }
 }
-console.log(obj.age);
-let{name,email,age,phoneno="1234"}=obj;
-console.log(`${name} and ${phoneno}`);
- 
+
+console.log(fun());
+
 
 
 
